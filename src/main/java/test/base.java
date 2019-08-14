cccd526472558b36f14a23032828c60ccfdc329a
smita <dsmita22@gmail.com>
2019-08-14 13:24:39 +1000

    first commit

---
+++ src/main/java/test/base.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class base {
+    @Test
+    public void demo(){
+        System.out.println("hello");
+    }
+    @Test
+    public void demo1(){
+        System.out.println("hello");
+    }
+
+
+}
